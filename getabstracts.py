b0aa79f9b3ceed65cf8d271f44ced6e3a3dea03c
Matt Deitke <mdeitke23@gmail.com>
2019-06-12 16:33:40 -0500

    Redo getabstracts.py to work with CVPR. Updates using string parsing.

--- getabstracts.py
+++ getabstracts.py
@@ -1,62 +1,33 @@
-# attempts to extract abstracts from the .pdfs 
-# this is a tricky process and will fail for some papers, in which
-# case the script tries to alert the user
-# this file is super hacky I'd adwise careful use
-
-# output is a set of files inside abstracts/ folder , one for each paper
+# From the CVPR website, this script will get the
+# abstract for each paper.
 
 import os
-from string import punctuation
-from operator import itemgetter
+import requests
 
-# get list of all PDFs supplied by NIPS
-relpath = "nips25offline/content/"
+# get list of all PDFs supplied by CVPR
+relpath = "content/"
 allFiles = os.listdir(relpath)
-pdfs = [x for x in allFiles if x.endswith(".pdf")]
+paperids = [x[:-4] for x in allFiles if x.endswith(".pdf")]
 
-for i,f in enumerate(pdfs):
-	paperid = f[9:-4]
-	fullpath = relpath + f
+# iterate over each paper
+for i, pid in enumerate(paperids):
+	# get the url to the page that contains the abstract
+	url = 'http://openaccess.thecvf.com/content_CVPR_2019/html/' + pid + '.html'
 
-	print "processing %s, %d/%d" % (paperid, i, len(pdfs))
+	print("Processing:", pid, i, '/', len(paperids)) # monitor progress
 
-	# create text file from the pdf contet
-	cmd = "pdftotext %s %s" % (fullpath, "out.txt")
-	#print "EXEC: " + cmd
-	os.system(cmd)
+	# get the abstract text from the html
+	abstract = requests.get(url).text
+	abstract = abstract[abstract.find('id="abstract"'):]
+	abstract = abstract[abstract.find('>') + 1:abstract.find('<')].strip()
 
-	txt = open("out.txt").read()
-	L = txt.split("\n")
+	# checks for empty abstract
+	# (often arises if link 404s)
+	if (len(abstract) == 0):
+		print('CHECK ABSTRACT FOR', pid)
+		continue
 
-	# basically, we find the line that says Abstract
-	# and then go down until we see an empty line
-	# i couldn't find a better way because there is a lot
-	# of variation. On NIPS 2012 papers, this fails for 
-	# 2 badly formatted papers, in which case the script
-	# alerts the user, I had to go in open that file manually
-	# and fix the abstract text inside it.
-	print "----------"
-	i=0
-	while (not L[i]=="Abstract") and i<len(L): i+=1
-	str = []
-	while (not L[i]=="") and i<len(L): 
-		i+=1
-		str.append(L[i])
-	abstract = " ".join(str)
-	print abstract
-	print len(abstract)
-	print "----------"
-
-	f = open("abstracts/a%d.txt" % (int(paperid), ), "w")
-	f.write(abstract);
+	# create new .txt file in abstracts folder
+	f = open("abstracts/" + pid + ".txt", "w")
+	f.write(abstract)
 	f.close()
-
-	# suspicious: this abstract is too long. Maybe its right,
-	# let the user deicde. If not, user can go in and fix the
-	# mistake manually
-	if len(abstract) > 2000:
-		print txt[:3000]
-		print paperid
-		a= raw_input()
-
-
